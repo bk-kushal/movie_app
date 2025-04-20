21a3f83fe705a853ccf7a06efd7e7c6e934b90af
bk-kushal <Kushal.Bk@Student.HTW-Berlin.de>
2025-04-20 16:20:29 +0200

    Initial commit with Spring Boot setup

---
+++ src/test/java/com/example/Movie_App/MovieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Movie_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
